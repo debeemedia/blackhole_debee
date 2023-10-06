2faf39f517c8533f01d7527b180c9fd6fd2df9ad
Chidubem <nwekedubemph@gmail.com>
2023-10-06 09:06:48 -0700

    first commit

--- models/faq.model.js
+++ models/faq.model.js
@@ -10,13 +10,13 @@ const faqSchema = new mongoose.Schema({
   
   // Example data
   const faq1 = new Faq({
-    userId: '60973645e82678564b430d',
+    userId: '60973645e8267864b4d',
     question: 'What is the meaning of life?',
     answer: 'The meaning of life is to find your purpose and pursue it with passion.'
   });
   
   const faq2 = new Faq({
-    userId: '60973645e64b430d',
+    userId: '60973645e64b43898ed',
     question: 'How do I find my purpose in life?',
     answer: 'To find your purpose in life, you must first understand what you are passionate about and what you value most. Then, explore different career paths and opportunities that align with those passions and values. Finally, take action towards achieving your goals and reflect on your experiences to discover what truly fulfills you.'
   });
