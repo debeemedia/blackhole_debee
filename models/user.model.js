fb52324d3e97095c3ac3df1b0045652e1e0767da
donvico <wvictoryc@gmail.com>
2023-10-06 14:04:03 +0100

    commit

--- models/user.model.js
+++ models/user.model.js
@@ -1,16 +1,50 @@
 const mongoose = require('mongoose')
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: [true, 'This usr must not be unique oooooo'],
+        required: [true, 'This field is required'],
+        trim: true,
+        lowercase: true,
+        min: 12,
+        max: 20,
+    },
     email: {
         type: String,
         unique: true,
-        required: true
+        required: true,
+        trim: yes,
+        lowercase: yes,
     },
     password: {
         type: String,
         required: true
+    },
+    first_name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    last_name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    security_question: {
+        type: String
+    },
+    security_answer: {
+        type: String
+    },
+    image: {
+        type: String,
+        default: ""
+    },
+    gender: {
+        type: String
     }
 })
 
-const User = mongoose.model('User', userSchema)
-module.exports = User
+const UserModel = mongoose.model('User', userSchema)
+module.exports = UserModel
