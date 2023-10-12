e9edc46f70933c8f70dd40df2391ab81f0191671
Echemartins <echemartins47@gmail.com>
2023-10-12 15:55:14 +0100

    completed sendEmail function

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ require('promise-dotenv').config()
 const express = require('express')
 const router = require('./routes')
 const connectToMongoDB = require('./db/connect')
+const sendEmail = require('./services/sendEmail')
 
 const app = express()
 const PORT = process.env.PORT || 5000
 const mongoURL = process.env.MONGODB_URL || 'please enter your mongo db connection string in the created .env file'
 
 app.use(express.json())
+
+
 
 app.get('/', (req, res)=>{
     res.send('You are on your way to building one of the most beautiful things the world has even seen.')
