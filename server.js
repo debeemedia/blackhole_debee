fc40efd52d5498b88db7ff9e290d3fbb649a2d16
Promise Johnson <promisej@phreetech.com>
2023-10-13 15:39:27 +0100

    added an email template folder

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('promise-dotenv').config()
 const express = require('express')
 const router = require('./routes')
 const connectToMongoDB = require('./db/connect')
+const { ROUTE_HOME } = require('./lib/page-route')
 
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -9,7 +10,7 @@ const mongoURL = process.env.MONGODB_URL || 'please enter your mongo db connecti
 
 app.use(express.json())
 
-app.get('/', (req, res)=>{
+app.get(ROUTE_HOME, (req, res)=>{
     res.send('You are on your way to building one of the most beautiful things the world has even seen.')
 })
 app.use('/api', router)
