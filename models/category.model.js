16eb34c5c88b6ea9e0cce7785789c359d07cfb76
Olubodun <bolowys33@gmail.com>
2023-10-15 14:18:47 +0100

    Create category controller

--- models/category.model.js
+++ models/category.model.js
@@ -1,5 +1,6 @@
 
 const mongoose = require('mongoose')
+const Products = require('./product_model')
 
 const categorySchema = new mongoose.Schema({
     name: {
@@ -8,7 +9,17 @@ const categorySchema = new mongoose.Schema({
     },
     description: {
         type: String
+    },
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
     }
+})
+
+categorySchema.pre('findByIdAndDelete', async function (next) {
+    category = this
+    Products.deleteMany({category: category._id})
+    next()
 })
 
 module.exports.CategoryModel = mongoose.model('category', categorySchema)
