a9e027fb879a1f18c913f3dab061183b8585c4ee
Deborah Okeke <okekedeborah@gmail.com>
2023-10-16 10:17:01 +0100

    implement email verification feature and update verification status of user in the database; also add route to issue new access token for authentication

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,21 +1,32 @@
 const User = require("../models/user.model")
+const { renderWelcomeMessage, sendMail } = require("../utils/mail")
 
 async function createUser(req,res){
     const{username, email, password, first_name, last_name, security_question, security_answer, image, gender} = req.body
-    if(!username, email, password, first_name, last_name, security_question, security_answer, image, gender) return res.status(401).json({success: false, message: 'fields should not be empty'})
+    if(!username || !email || !password || !first_name || !last_name) return res.status(400).json({success: false, message: 'required fields should not be empty'})
     try {
         const emailExist = await User.findOne({email: email})
         if (emailExist){
-        res.status(401).json({success: false, message: 'email already exist'})
+        res.status(400).json({success: false, message: 'email already exists'})
         }
         else{
-            const newUser = new User({username, email, password, first_name, last_name, security_question, security_answer, image, gender})
+            const newUser = await new User({username, email, password, first_name, last_name, security_question, security_answer, image, gender})
             await newUser.save()
-            res.status(200).json({success: true, message: 'user created succesfully'})
+
+            // send a mail to user on successful registration
+            const emailOption = {
+                to: email,
+                from: 'Aphia',
+                subject: 'Registration Successful',
+                html: await renderWelcomeMessage(first_name, newUser)
+            }
+            await sendMail(emailOption, res)
+
+            res.status(201).json({success: true, message: 'user created succesfully'})
         }
     } catch (error) {
         console.error(error);
-        res.status(401).json({success: false, message: 'unable to register user'})
+        res.status(500).json({success: false, message: 'internal server error'})
     }
 }
 async function getUsers(req, res){
