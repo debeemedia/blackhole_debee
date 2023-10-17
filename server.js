d0149928151cfba22ad898f30d450c4d21fd6ca5
Promise Johnson <promisej@phreetech.com>
2023-10-17 17:08:00 +0100

    update on dev

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 require('dotenv').config()
-const fs = require('fs')
 const express = require('express')
 const router = require('./routes')
 const connectToMongoDB = require('./db/connect')
 const { ROUTE_HOME } = require('./lib/page-route')
 const path = require('path')
-const mailBuilder = require('./utils/mailBuilder')
 const { sendMail, buildEmailTemplate } = require('./utils/mail')
 
 const app = express()
@@ -18,12 +16,12 @@ console.log(__dirname)
 app.get(ROUTE_HOME, async(req, res)=>{
 
     //This is how we would use to send emails
-    const emailResult = await sendMail({
-        to: 'chiemelapromise30@gmail.com',
-         from: 'admin',
-          subject: 'Confirmation Email',
-           html: await buildEmailTemplate('request_password_reset.ejs', {first_name: 'Promise', email: 'chiemelapromise30@gmail.com'})
-    })
+    // const emailResult = await sendMail({
+    //     to: 'chiemelapromise30@gmail.com',
+    //      from: 'admin',
+    //       subject: 'Confirmation Email',
+    //        html: await buildEmailTemplate('request_password_reset.ejs', {first_name: 'Promise', email: 'chiemelapromise30@gmail.com'})
+    // })
     return res.send('You are on your way to building one of the most beautiful things the world has even seen.')
 })
 app.set('views', './views')
@@ -31,6 +29,6 @@ app.set('view engine', 'ejs')
 app.use('/api', router)
 
 app.listen(PORT, async ()=>{
-    await connectToMongoDB(mongoURL)
     console.log(`Server running on port ${PORT}`)
+    await connectToMongoDB(mongoURL)
 })
