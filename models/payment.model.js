f05e8e07a95f48ffd11bb026de68c0c7e9fb1143
Deborah Okeke <okekedeborah@gmail.com>
2023-10-21 15:18:43 +0100

    implement payment schema

---
+++ models/payment.model.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+const paymentSchema = mongoose.Schema({
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    order_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Order',
+        required: true
+    },
+    transaction_id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    payment_gateway: {
+        type: String,
+        required: true,
+        default: 'Flutterwave'
+    },
+    payment_method: {
+        type: String,
+        required: true,
+        default: 'Credit Card'
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    currency: {
+        type: String,
+        required: true,
+        default: 'NGN'
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'completed', 'failed'],
+        required: true
+    }
+}, {timestamps: true})
+
+const PaymentModel = mongoose.model('Payment', paymentSchema)
+module.exports = PaymentModel
