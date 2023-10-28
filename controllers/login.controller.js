dc4fe9f59aede0327a8c472dc976629ad170762f
Deborah Okeke <okekedeborah@gmail.com>
2023-10-28 14:23:08 +0100

    add role in jwt and protect route for product creation; link product creation to selected category

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -24,9 +24,9 @@ async function login (req, res) {
                 }
 
                 // issue access token
-                const accessToken = jwt.sign({id: user._id, email: user.email, username: user.username}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '10m'})
+                const accessToken = jwt.sign({id: user._id, email: user.email, username: user.username, role: user.role}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '10m'})
                 // issue refresh token
-                const refreshToken = jwt.sign({id: user._id, email: user.email, username: user.username}, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '1h'})
+                const refreshToken = jwt.sign({id: user._id, email: user.email, username: user.username, role: user.role}, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '1h'})
                 // set refresh token in cookie
                 res.cookie('jwt', refreshToken, {httpOnly: true, sameSite: 'None', secure: true, maxAge: 60 * 60 * 1000})
 
