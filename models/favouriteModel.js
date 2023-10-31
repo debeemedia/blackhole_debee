9b0541b78bb723c0d34c65daa83b1934c1c206a4
Echemartins <echemartins47@gmail.com>
2023-10-31 16:07:19 +0000

    completed favourites controller

--- models/favouriteModel.js
+++ models/favouriteModel.js
@@ -1,24 +1,18 @@
 const mongoose = require('mongoose')
 
 const favouritesSchema = new mongoose.Schema({
-    userId : {
+    user : {
         type:mongoose.Schema.Types.ObjectId,
         required: true,
         ref: 'User'
     },
-    favproduct: [{
-        product: {
-            type: mongoose.Schema.Types.ObjectId,
-            required: true,
-            ref: 'product'
-        },
-        addedAt: {
-            type: Date,
-            default: Date.now 
-        }
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'product'
     }
-    ]
-})
+},{timestamps: true}
+)
 
 const Favourite = mongoose.model('favourite', favouritesSchema)
 
