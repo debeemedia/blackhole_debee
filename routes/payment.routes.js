1035d177ae01facc9a75b5a79fb93b4252efe28d
Deborah Okeke <okekedeborah@gmail.com>
2023-11-04 09:05:18 +0100

    refactor payment route

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const { listenWebhook } = require('../controllers/payment.controller')
+const { ROUTE_PAYMENT_WEBHOOK } = require('../lib/page-route')
 const router = express.Router()
 
-router.post('/webhook/flutterwave', listenWebhook)
+router.post(ROUTE_PAYMENT_WEBHOOK, listenWebhook)
 
 module.exports.paymentRouter = router
