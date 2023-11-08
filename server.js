f20d459aad27603606498ecc78fe5e0470c9caeb
Olubodun <bolowys33@gmail.com>
2023-11-08 11:42:03 +0100

    Update cors in server

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ app.use(express.static(path.join(__dirname, 'public')))
 const PORT = process.env.PORT || 5000
 const mongoURL = process.env.MONGODB_URL || 'please enter your mongo db connection string in the created .env file'
 
-app.use(cors())
+app.use(cors({
+    origin: '*',
+    credentials: true
+}))
 app.use(cookieParser())
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
