3fdd03af20f4834422c2bd6cc9b676e5f00a7e0b
Deborah Okeke <okekedeborah@gmail.com>
2023-11-10 20:20:59 +0100

    send specific error messages in verify bank controller

--- controllers/verify.bank.controller.js
+++ controllers/verify.bank.controller.js
@@ -5,9 +5,11 @@ const validateData = require("../utils/validate");
 
 async function verifyBank (req, res) {
     try {
+        // destructure request body
         const {account_number, account_bank} = req.body
         const flw = new Flutterwave('public key', process.env.FLUTTERWAVE_SECRET_KEY);
-       
+        
+        // validate input
         const validateRule = {
             account_number: "string|required",
             account_bank: "string|required"
@@ -21,11 +23,25 @@ async function verifyBank (req, res) {
           if (!validateResult.success) {
             return res.json(validateResult.data);
         }
-        // const payload = {"account_number": "0690000032", "account_bank": "044"};
+        // check that account bank (the bank code) length is 3 characters else flutterwave will throw eror
+        if (account_bank.length !== 3) {
+            return res.json({success: false, message: 'account_bank length must be 3 characters long'})
+        }
+
+        // verify account number with flutterwave SDK and send a response
         const payload = {'account_number': account_number, 'account_bank': account_bank}
         const response = await flw.Misc.verify_Account(payload);
+
         // console.log(response);
-        res.json({success: true, message: response.data})
+        if (response && response.status === 'success') {
+            return res.json({success: true, message: response.data})
+        } else if (response.status === 'error') {
+            return res.json({success: false, message: response.message})
+            // // some error messages from flutterwave
+            //     message: 'Sorry, that account number is invalid, please check and try again'
+            //     message: 'Unknown Bank Code'
+              
+        }
         
     } catch (error) {
         console.log(error.message);
