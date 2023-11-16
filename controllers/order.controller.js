cc9240377212238e32740c2b045d8c9f4867b0f8
Olubodun <bolowys33@gmail.com>
2023-11-16 15:42:18 +0100

    Implement order details relating to a vendor

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -2,6 +2,7 @@ const OrderModel = require("../models/order.model");
 const validateData = require("../utils/validate");
 const { empty } = require("../utils/helpers");
 const PaymentModel = require("../models/payment.model");
+const ProductModel = require("../models/product.model");
 
 async function createOrder(req, res, next) {
     const user_id = req.user.id
@@ -220,11 +221,44 @@ async function markDelivered(req, res) {
     }
 }
 
+async function getAllOrdersForVendor(req, res) {
+    try {
+        const {id} = req.user
+        const orders = await OrderModel.find({completed: true})
+
+        if (orders.length == 0) {
+            return res.json({success: false, message: `No order present`})
+        }
+
+        const vendorOrders = []
+
+        for (const order of orders) {
+            const order_id = order._id
+
+            for (const product of order.products) {
+                const productDetails = await ProductModel.findById(product.product_id)
+                
+                if (productDetails.user_id != id) {
+                   continue
+                }
+
+                const newDetails = {product_name: productDetails.name, quantity: product.quantity, order_id}
+                vendorOrders.push(newDetails)
+            }
+        }
+
+        res.json({success: true, message: vendorOrders})
+    } catch (error) {
+        res.json({success: false, error: error.message})
+    }
+}
+
 module.exports = {
     markDelivered,
     createOrder,
     getAllOrdersByUser,
     getAllOrders,
     getAnOrderByUser,
-    deleteOrder
+    deleteOrder,
+    getAllOrdersForVendor
 }
--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,9 +1,10 @@
 
 const express = require('express')
-const { ROUTE_ORDER_CREATE, ROUTE_ORDER_DELIVERED, ROUTE_ORDER_ALL_BY_USER, ROUTE_ORDER_ONE_BY_USER, ROUTE_ORDER_ALL, ROUTE_ORDER_DELETE } = require('../lib/page-route')
-const { createOrder, markDelivered, getAllOrdersByUser, getAnOrderByUser, getAllOrders, deleteOrder } = require('../controllers/order.controller')
+const { ROUTE_ORDER_CREATE, ROUTE_ORDER_DELIVERED, ROUTE_ORDER_ALL_BY_USER, ROUTE_ORDER_ONE_BY_USER, ROUTE_ORDER_ALL, ROUTE_ORDER_DELETE, ROUTE_ORDER_VENDOR_ORDERS } = require('../lib/page-route')
+const { createOrder, markDelivered, getAllOrdersByUser, getAnOrderByUser, getAllOrders, deleteOrder, getAllOrdersForVendor } = require('../controllers/order.controller')
 const { initiatePayment } = require('../controllers/payment.controller')
 const { authenticate } = require('../middleware/auth')
+const { checkVendor } = require('../middleware/check.vendor')
 const router = express.Router()
 
 
@@ -13,6 +14,7 @@ router.get(ROUTE_ORDER_ALL_BY_USER, authenticate, getAllOrdersByUser)
 router.get(ROUTE_ORDER_ONE_BY_USER, authenticate, getAnOrderByUser)
 router.get(ROUTE_ORDER_ALL, getAllOrders)
 router.delete(ROUTE_ORDER_DELETE, authenticate, deleteOrder)
+router.get(ROUTE_ORDER_VENDOR_ORDERS, authenticate, checkVendor, getAllOrdersForVendor)
 
 
 module.exports.orderRouter = router
